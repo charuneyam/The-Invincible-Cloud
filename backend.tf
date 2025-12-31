1f0392c94d66f9021dd0b3581f15f07e1bd0560c
Rohith Kalluraya K <rohithkallurayak@Mac.lan>
2025-12-31 20:39:57 +0530

    Initialize containerization with GHCR

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "invincible-cloud-terraform-state" # Bucket name
-    key            = "terraformrohbranch.tfstate"                # The path inside the bucket
+    key            = "terraform.tfstate"                # The path inside the bucket
     region         = "us-east-1"
     dynamodb_table = "invincible-cloud-lock-table"
     encrypt        = true
